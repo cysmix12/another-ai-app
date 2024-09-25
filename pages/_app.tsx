4a368d714f3d8af5a14768b205c1e402ea09376a
cysmix12 <rjsocs@gmail.com>
2024-09-25 21:40:09 +0800

    feat: default files cleanup and mantine setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,28 @@
 import '@mantine/core/styles.css';
 
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
 import { createTheme, MantineProvider } from '@mantine/core';
 
-const theme = createTheme({});
+const primaryFont = Inter({ subsets: ['latin'] });
 
-export default function App({ Component, pageProps }: AppProps) {
+const theme = createTheme({
+  scale: 1,
+  fontFamily: primaryFont.style.fontFamily,
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <MantineProvider theme={theme}>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <>
+      <Head>
+        <title>Another AI App</title>
+      </Head>
+      <MantineProvider theme={theme} defaultColorScheme="dark">
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
   );
-}
+};
+
+export default App;
